b59e44e5148bddf90c97a5ff5f361305dcb57578
Erik Dalén <erik.gustav.dalen@gmail.com>
2022-02-05 16:16:02 +0100

    configure to use prod api

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import {
 } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5156/graphql',
+  uri: 'https://production-api.waremu.com/graphql',
   cache: new InMemoryCache(),
 });
 
