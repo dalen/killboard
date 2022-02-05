9b0d5a8c2270ed72e29b93d67aa8c8d3911f3ee2
Erik Dalén <erik.gustav.dalen@gmail.com>
2022-02-05 17:44:22 +0100

    Shared kills list component

---
+++ src/components/KillsList.tsx
@@ -0,0 +1,120 @@
+import { DocumentNode, useQuery } from '@apollo/client';
+import { format, formatISO } from 'date-fns';
+import { Link } from 'react-router-dom';
+import { Query } from '../types';
+import { CareerIcon } from './CareerIcon';
+
+export const KillsList = ({
+  query,
+  title,
+}: {
+  query: DocumentNode;
+  title: string;
+}): JSX.Element => {
+  const { loading, error, data } = useQuery<Query>(query);
+
+  if (loading) return <p>Loading...</p>;
+  if (error)
+    return (
+      <div>
+        <p>Error :(</p>
+        <pre>{error.name}</pre>
+        <pre>{error.message}</pre>
+      </div>
+    );
+
+  if (data?.kills?.nodes == null) return <p>error</p>;
+
+  return (
+    <div>
+      <div className="is-size-4 is-family-secondary is-uppercase">{title}</div>
+      <table className="table is-striped is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Killer</th>
+            <th>Victim</th>
+            <th>Location</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.kills.nodes.map((kill) => {
+            const date = new Date(kill.time * 1000);
+
+            return (
+              <tr key={kill.id}>
+                <td>
+                  {formatISO(date, { representation: 'date' })}
+                  <br />
+                  {format(date, 'HH:mm:ss')}
+                </td>
+                <td>
+                  <article className="media">
+                    <figure className="media-left">
+                      <CareerIcon career={kill.attackers[0].character.career} />
+                    </figure>
+                    <div className="media-content">
+                      <div className="content">
+                        <Link
+                          to={`/character/${kill.attackers[0].character.id}`}
+                        >
+                          <strong>{kill.attackers[0].character.name}</strong>
+                        </Link>
+                        <br />
+                        <Link to={`/guild/${kill.attackers[0].guild?.id}`}>
+                          {kill.attackers[0].guild?.name}
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="media-right">
+                      <small>
+                        Lvl {kill.attackers[0].level}
+                        <br />
+                        RR {kill.attackers[0].renownRank}
+                      </small>
+                    </div>
+                  </article>
+                </td>
+                <td>
+                  <article className="media">
+                    <figure className="media-left">
+                      <CareerIcon career={kill.victim.character.career} />
+                    </figure>
+                    <div className="media-content">
+                      <div className="content">
+                        <Link to={`/character/${kill.victim.character.id}`}>
+                          <strong>{kill.victim.character.name}</strong>
+                        </Link>
+                        <br />
+                        <Link to={`/guild/${kill.victim.guild?.id}`}>
+                          {kill.victim.guild?.name}
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="media-right">
+                      <small>
+                        Lvl {kill.victim.level}
+                        <br />
+                        RR {kill.victim.renownRank}
+                      </small>
+                    </div>
+                  </article>
+                </td>
+                <td>
+                  Zone: {kill.position?.zoneId}
+                  {kill.scenarioId !== 0 && <p>Scenario: {kill.scenarioId}</p>}
+                </td>
+                <td>
+                  <Link to={`/kill/${kill.id}`} className="button is-primary">
+                    Details
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
