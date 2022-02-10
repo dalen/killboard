a4b90b8f8b020261abc62fa2f590120d50380b0e
Erik Dalén <erik.gustav.dalen@gmail.com>
2022-02-11 00:00:17 +0100

    remove pagination config to use all as key args for cache again

--- src/index.tsx
+++ src/index.tsx
@@ -4,27 +4,10 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import './style.scss';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { relayStylePagination } from '@apollo/client/utilities';
 
 const client = new ApolloClient({
   uri: 'https://production-api.waremu.com/graphql',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          guildFeudKills: relayStylePagination(),
-          playerFeudKills: relayStylePagination(),
-          kills: relayStylePagination(),
-          characters: relayStylePagination(),
-        },
-      },
-      Guild: {
-        fields: {
-          members: relayStylePagination(),
-        },
-      },
-    },
-  }),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
