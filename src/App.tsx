520235855400002efecb41dce1bfd5cd0cb1bc29
Emissaryx <tcates79@gmail.com>
2026-08-13 11:44:04 -0400

    Add shared navigation and consistent page styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './i18n/config';
 import React from 'react';
 import { Character } from '@/pages/Character';
 import { Home } from '@/pages/Home';
+import { MainNav } from '@/components/global/MainNav';
 import { Kill } from '@/pages/Kill';
 import { Guild } from '@/pages/Guild';
 import { Search } from '@/pages/Search';
@@ -54,103 +55,121 @@ const App = () => {
   usePageViews();
 
   return (
-    <Routes>
-      <Route path="/" element={<Home tab="players" />} />
-      <Route path="/guilds" element={<Home tab="guilds" />} />
-      <Route path="/scenarios" element={<Home tab="scenarios" />} />
-      <Route path="/kill/:id" element={<Kill />} />
-      <Route path="/kills" element={<Kills />} />
-      <Route path="/character/:id" element={<Character tab="kills" />} />
-      <Route
-        path="/character/:id/scenarios"
-        element={<Character tab="scenarios" />}
-      />
-      <Route
-        path="/character/:id/skirmishes"
-        element={<Character tab="skirmishes" />}
-      />
-      <Route
-        path="/character/:id/armory"
-        element={<Character tab="armory" />}
-      />
-      <Route
-        path="/character/:playerId1/feud/:playerId2"
-        element={<PlayerFeudPage />}
-      />
+    <>
+      <MainNav />
+      <Routes>
+        <Route path="/" element={<Home tab="players" />} />
+        <Route path="/guilds" element={<Home tab="guilds" />} />
+        <Route path="/scenarios" element={<Home tab="scenarios" />} />
+        <Route path="/kill/:id" element={<Kill />} />
+        <Route path="/kills" element={<Kills />} />
+        <Route path="/character/:id" element={<Character tab="kills" />} />
+        <Route
+          path="/character/:id/scenarios"
+          element={<Character tab="scenarios" />}
+        />
+        <Route
+          path="/character/:id/skirmishes"
+          element={<Character tab="skirmishes" />}
+        />
+        <Route
+          path="/character/:id/armory"
+          element={<Character tab="armory" />}
+        />
+        <Route
+          path="/character/:playerId1/feud/:playerId2"
+          element={<PlayerFeudPage />}
+        />
 
-      <Route path="/creatures" element={<Creatures />} />
-      <Route path="/creature/:id" element={<Creature />} />
-      <Route path="/creature/:id/quests" element={<Creature tab="quests" />} />
-      <Route
-        path="/creature/:id/vendorItems"
-        element={<Creature tab="vendorItems" />}
-      />
-      <Route path="/creature/:id/zone/:zoneId" element={<Creature />} />
+        <Route path="/creatures" element={<Creatures />} />
+        <Route path="/creature/:id" element={<Creature />} />
+        <Route
+          path="/creature/:id/quests"
+          element={<Creature tab="quests" />}
+        />
+        <Route
+          path="/creature/:id/vendorItems"
+          element={<Creature tab="vendorItems" />}
+        />
+        <Route path="/creature/:id/zone/:zoneId" element={<Creature />} />
 
-      <Route path="/gameobject/:id" element={<GameObject />} />
-      <Route path="/creature/:id/quests" element={<GameObject />} />
-      <Route path="/creature/:id/zone/:zoneId" element={<GameObject />} />
+        <Route path="/gameobject/:id" element={<GameObject />} />
+        <Route path="/creature/:id/quests" element={<GameObject />} />
+        <Route path="/creature/:id/zone/:zoneId" element={<GameObject />} />
 
-      <Route path="/guild/:id" element={<Guild tab="kills" />} />
-      <Route path="/guild/:id/members" element={<Guild tab="members" />} />
-      <Route path="/guild/:id/scenarios" element={<Guild tab="scenarios" />} />
-      <Route
-        path="/guild/:id/skirmishes"
-        element={<Guild tab="skirmishes" />}
-      />
-      <Route
-        path="/guild/:guildId1/feud/:guildId2"
-        element={<GuildFeudPage />}
-      />
+        <Route path="/guild/:id" element={<Guild tab="kills" />} />
+        <Route path="/guild/:id/members" element={<Guild tab="members" />} />
+        <Route
+          path="/guild/:id/scenarios"
+          element={<Guild tab="scenarios" />}
+        />
+        <Route
+          path="/guild/:id/skirmishes"
+          element={<Guild tab="skirmishes" />}
+        />
+        <Route
+          path="/guild/:guildId1/feud/:guildId2"
+          element={<GuildFeudPage />}
+        />
 
-      <Route path="/quests" element={<Quests />} />
-      <Route path="/quest/:id" element={<Quest />} />
+        <Route path="/quests" element={<Quests />} />
+        <Route path="/quest/:id" element={<Quest />} />
 
-      <Route path="/search/:query" element={<Search />} />
-      <Route path="/search/guild/:query" element={<SearchGuild />} />
+        <Route path="/search/:query" element={<Search />} />
+        <Route path="/search/guild/:query" element={<SearchGuild />} />
 
-      <Route path="/scenario/:id" element={<Scenario tab="scoreboard" />} />
-      <Route path="/scenario/:id/kills" element={<Scenario tab="kills" />} />
-      <Route
-        path="/scenario/:id/skirmishes"
-        element={<Scenario tab="skirmishes" />}
-      />
-      <Route path="/scenario/:id/map" element={<Scenario tab="map" />} />
-      <Route path="/skirmishes" element={<Home tab="skirmishes" />} />
-      <Route path="/skirmish/:id" element={<Skirmish tab="scoreboard" />} />
-      <Route path="/skirmish/:id/kills" element={<Skirmish tab="kills" />} />
-      <Route
-        path="/skirmish/:id/damage/:characterId"
-        element={<Skirmish tab="characterDamage" />}
-      />
-      <Route path="/skirmish/:id/damage" element={<Skirmish tab="damage" />} />
-      <Route path="/zone_heatmap/:id" element={<ZoneMap />} />
+        <Route path="/scenario/:id" element={<Scenario tab="scoreboard" />} />
+        <Route path="/scenario/:id/kills" element={<Scenario tab="kills" />} />
+        <Route
+          path="/scenario/:id/skirmishes"
+          element={<Scenario tab="skirmishes" />}
+        />
+        <Route path="/scenario/:id/map" element={<Scenario tab="map" />} />
+        <Route path="/skirmishes" element={<Home tab="skirmishes" />} />
+        <Route path="/skirmish/:id" element={<Skirmish tab="scoreboard" />} />
+        <Route path="/skirmish/:id/kills" element={<Skirmish tab="kills" />} />
+        <Route
+          path="/skirmish/:id/damage/:characterId"
+          element={<Skirmish tab="characterDamage" />}
+        />
+        <Route
+          path="/skirmish/:id/damage"
+          element={<Skirmish tab="damage" />}
+        />
+        <Route path="/zone_heatmap/:id" element={<ZoneMap />} />
 
-      <Route path="/items" element={<Items />} />
-      <Route path="/item/:id" element={<Item tab="vendors" />} />
-      <Route path="/item/:id/purchase" element={<Item tab="purchase" />} />
-      <Route path="/item/:id/quests" element={<Item tab="quests" />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/item/:id" element={<Item tab="vendors" />} />
+        <Route path="/item/:id/purchase" element={<Item tab="purchase" />} />
+        <Route path="/item/:id/quests" element={<Item tab="quests" />} />
 
-      <Route path="/instance-runs" element={<InstanceRuns />} />
-      <Route path="/instance-run/:id" element={<InstanceRun />} />
-      <Route
-        path="/instance-run/:instanceRunId/:id"
-        element={<InstanceEncounterRun />}
-      />
+        <Route path="/instance-runs" element={<InstanceRuns />} />
+        <Route path="/instance-run/:id" element={<InstanceRun />} />
+        <Route
+          path="/instance-run/:instanceRunId/:id"
+          element={<InstanceEncounterRun />}
+        />
 
-      <Route path="/instances" element={<Instances />} />
-      <Route path="/instance-statistics/:id" element={<InstanceStatistics />} />
+        <Route path="/instances" element={<Instances />} />
+        <Route
+          path="/instance-statistics/:id"
+          element={<InstanceStatistics />}
+        />
 
-      <Route path="/storylines" element={<Storylines />} />
-      <Route path="/storylines/:id" element={<Storyline />} />
-      <Route path="/storylines/:storylineId/:id" element={<StorylineEntry />} />
-      <Route
-        path="/storylines/:storylineId/:storylineEntryId/:id"
-        element={<StorylineActivity />}
-      />
+        <Route path="/storylines" element={<Storylines />} />
+        <Route path="/storylines/:id" element={<Storyline />} />
+        <Route
+          path="/storylines/:storylineId/:id"
+          element={<StorylineEntry />}
+        />
+        <Route
+          path="/storylines/:storylineId/:storylineEntryId/:id"
+          element={<StorylineActivity />}
+        />
 
-      <Route path="/ranked-leaderboard" element={<RankedLeaderboard />} />
-    </Routes>
+        <Route path="/ranked-leaderboard" element={<RankedLeaderboard />} />
+      </Routes>
+    </>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,3 @@
-import clsx from 'clsx';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router';
 import { LatestKills } from '@/components/kill/LatestKills';
 import { WeeklyLeaderboard } from '@/components/kill/WeeklyLeaderboard';
 import { SearchBox } from '@/components/global/SearchBox';
@@ -18,24 +15,8 @@ export const Home = ({
 }: {
   tab: 'players' | 'guilds' | 'scenarios' | 'skirmishes';
 }): ReactElement => {
-  const { t } = useTranslation();
-
   return (
-    <div className="container is-mobile mt-2">
-      <div className="tabs is-fullwidth">
-        <li className={clsx({ 'is-active': tab === 'players' })}>
-          <Link to="/">{t('pages:home.showPlayerLeaderboard')}</Link>
-        </li>
-        <li className={clsx({ 'is-active': tab === 'guilds' })}>
-          <Link to="/guilds">{t('pages:home.showGuildLeaderboard')}</Link>
-        </li>
-        <li className={clsx({ 'is-active': tab === 'scenarios' })}>
-          <Link to="/scenarios">{t('pages:home.showScenarios')}</Link>
-        </li>
-        <li className={clsx({ 'is-active': tab === 'skirmishes' })}>
-          <Link to="/skirmishes">{t('pages:home.showSkirmishes')}</Link>
-        </li>
-      </div>
+    <div className="container is-max-widescreen mt-2">
       {tab === 'scenarios' && (
         <>
           <ScenarioFilters />
